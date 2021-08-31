f4c99cf64a58e602adb904f0b57ab3c6cd340fa9
Sami Panseri <sami.panseri@epfl.ch>
2021-08-31 10:55:26 +0200

    Ajout des derniers liens go

--- index.js
+++ index.js
@@ -4,4 +4,25 @@ const xml2js = require('xml2js');
 
 const bot = new Telegraf(process.env.BOTONEWSTOKEN);
 
+bot.command('getfeed', (ctx) => {
+    fetch('https://go.epfl.ch/feed')
+    .then(res => res.text())
+    .then(body => {
+        xml2js.parseString(body, (err, result) => {
+            if(err) {
+                throw err;
+            }
+
+            const json = JSON.stringify(result, null, 4);
+            const superjson = JSON.parse(json)
+            //console.log(superjson.feed.entry)
+
+            let messages = `Les derniers liens raccourcis sur go.epfl.ch\n\n`
+
+             for (let i = 0; i != 3; i++) {
+                messages = messages + `${superjson.feed.entry[i].title[0]._}\n\n`
+            }
+            ctx.telegram.sendMessage(ctx.message.chat.id, `${messages}`)
+        })
+    })
 bot.launch();
