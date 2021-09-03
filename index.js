3710c345b2d71dbb9bd940d07c281ff4f44d0e42
Sami Panseri <sami.panseri@epfl.ch>
2021-09-03 13:14:32 +0200

    Made a function to fetch tweets

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const { Telegraf } = require('telegraf')
 const fetch = require('node-fetch');
 const xml2js = require('xml2js');
+const config = require("./config.json")
 
-if(!process.env.BOTONEWSTOKEN) {
+var fetchtweets = require("./lib/fetchtweets.js")
+
+if(!config.BOTONEWSTOKEN) {
     return console.error("Please set BOTONEWSTOKEN")
 }
-if(!process.env.BOTONEWSTWITTERTOKEN) {
+if(!config.BOTONEWSTWITTERTOKEN) {
     return console.error("Please set BOTONEWSTWITTERTOKEN")
 }
 
-const bot = new Telegraf(process.env.BOTONEWSTOKEN);
+const bot = new Telegraf(config.BOTONEWSTOKEN);
 
 const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
@@ -18,7 +21,7 @@ var tweetsneckbeard = `[@neckbeardhacker](https://twitter.com/NeckbeardHacker) l
 var tweetshipster = `[@hispterhacker](https://twitter.com/hipsterhacker) last tweets :\n\n\n`
 var newsepfl = `Les dernières news de l'EPFL\n\n`
 
-bot.command('getfeed', (ctx) => {
+bot.command('getfeed', async (ctx) => {
     fetch('https://go.epfl.ch/feed')
     .then(res => res.text())
     .then(body => {
@@ -38,6 +41,7 @@ bot.command('getfeed', (ctx) => {
             }
             ctx.telegram.sendMessage(ctx.message.chat.id, `${messages}`)
 
+            
             fetch('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty')
                 .then(res => res.json())
                 .then(body => {
@@ -57,41 +61,54 @@ bot.command('getfeed', (ctx) => {
                       });
                 })
 
-                fetch('https://api.twitter.com/2/users/278523798/tweets', { 
-                    method: 'get', 
-                    headers:{'Authorization': `Bearer ${process.env.BOTONEWSTWITTERTOKEN}`}
-                    })
-                    .then(res => res.json())
-                    .then(body => {
+                //tweetsneckbeard = tweetsneckbeard + fetchtweets("278523798", "neckbeardhacker");
+                fetchtweets("278523798", "neckbeardhacker").then(function(result) {
+                    tweetsneckbeard = tweetsneckbeard + result
+                })
+                //console.log(tweetsneckbeard)
+                // fetch('https://api.twitter.com/2/users/278523798/tweets', { 
+                //     method: 'get', 
+                //     headers:{'Authorization': `Bearer ${config.BOTONEWSTWITTERTOKEN}`}
+                //     })
+                //     .then(res => res.json())
+                //     .then(body => {
 
-                        for (let i = 0; i != 3; i++) {
-                            tweetsneckbeard = tweetsneckbeard + `[${body.data[i].text}](https://twitter.com/NeckBeardHacker/status/${body.data[i].id})\n\n===================\n\n`
-                        }
-                    })
+                //         for (let i = 0; i != 3; i++) {
+                //             tweetsneckbeard = tweetsneckbeard + `[${body.data[i].text}](https://twitter.com/NeckBeardHacker/status/${body.data[i].id})\n\n===================\n\n`
+                //         }
+                //     })
 
-                fetch('https://api.twitter.com/2/users/261546340/tweets', { 
-                    method: 'get', 
-                    headers:{'Authorization': `Bearer ${process.env.BOTONEWSTWITTERTOKEN}`}
-                    })
-                    .then(res => res.json())
-                    .then(body => {
+                fetchtweets("261546340", "hipsterhacker").then(function(result) {
+                    tweetshipster = tweetshipster + result
+                })
 
-                        for (let i = 0; i != 3; i++) {
-                            tweetshipster = tweetshipster + `[${body.data[i].text}](https://twitter.com/hipsterhacker/status/${body.data[i].id})\n\n===================\n\n`
-                        }
-                    })
+                // fetch('https://api.twitter.com/2/users/261546340/tweets', { 
+                //     method: 'get', 
+                //     headers:{'Authorization': `Bearer ${config.BOTONEWSTWITTERTOKEN}`}
+                //     })
+                //     .then(res => res.json())
+                //     .then(body => {
 
-                fetch('https://api.twitter.com/2/users/2317524115/tweets', { 
-                    method: 'get', 
-                    headers:{'Authorization': `Bearer ${process.env.BOTONEWSTWITTERTOKEN}`}
-                    })
-                    .then(res => res.json())
-                    .then(body => {
+                //         for (let i = 0; i != 3; i++) {
+                //             tweetshipster = tweetshipster + `[${body.data[i].text}](https://twitter.com/hipsterhacker/status/${body.data[i].id})\n\n===================\n\n`
+                //         }
+                //     })
 
-                        for (let i = 0; i != 3; i++) {
-                            tweetsphp = tweetsphp + `[${body.data[i].text}](https://twitter.com/php_ceo/status/${body.data[i].id})\n\n===================\n\n`
-                        }
-                    })
+                fetchtweets("2317524115", "php_ceo").then(function(result) {
+                    tweetsphp = tweetsphp + result
+                })
+
+                // fetch('https://api.twitter.com/2/users/2317524115/tweets', { 
+                //     method: 'get', 
+                //     headers:{'Authorization': `Bearer ${config.BOTONEWSTWITTERTOKEN}`}
+                //     })
+                //     .then(res => res.json())
+                //     .then(body => {
+
+                //         for (let i = 0; i != 3; i++) {
+                //             tweetsphp = tweetsphp + `[${body.data[i].text}](https://twitter.com/php_ceo/status/${body.data[i].id})\n\n===================\n\n`
+                //         }
+                //     })
                     
                     sleep(1000).then(() => {
                         ctx.telegram.sendMessage(ctx.message.chat.id, `${tweetsneckbeard}${tweetshipster}${tweetsphp}`, { parse_mode: 'Markdown' })
@@ -105,18 +122,14 @@ bot.command('getfeed', (ctx) => {
                 })
                 .then(res => res.json())
                 .then(body => {
-
                     for (let i = 0; i != 3; i++) {
                         newsepfl = newsepfl + `[${body.results[i].title}](${body.results[i].news_url})\n\n`
                     }
                 })
-
                     sleep(1000).then(() => {
                         ctx.telegram.sendMessage(ctx.message.chat.id, `${newsepfl}`, { parse_mode: 'Markdown' })
                     });
                     newsepfl = `Les dernières news de l'EPFL\n\n`
-
-
                 })
         })
     })
